ff5da04cca4083235c37cde4e5569cf0186b78b5
Justin White <kyjus25@gmail.com>
2022-09-01 17:21:22 -0500

    Quicksave

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ server.get('/:user/:repo', async (request) => {
     const { user, repo } = request.params as {user: string, repo: string};
     const numCommits = await axios.get(`https://api.github.com/repos/${user}/${repo}/commits?per_page=1`).then(i => i.headers.link?.replace('>; rel=\"last\"', '').match(/\d+$/)?.[0]);
     const firstCommit = await axios.get(`https://api.github.com/repos/${user}/${repo}/commits?per_page=1&page=${numCommits}`).then(i => i.data);
-    return { hello: 'world', user, repo, numCommits, firstCommit}
+    return { numCommits, firstCommit}
 });
 
 server.listen({port: 3000});
