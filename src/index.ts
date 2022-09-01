1f274af1564b740b050fa1371f39fbe4aaeefac9
Justin White <kyjus25@gmail.com>
2022-09-01 17:22:44 -0500

    Clean up

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
 import fastify from 'fastify';
 import axios from 'axios';
-import * as cheerio from 'cheerio';
 
 const server = fastify();
 
-console.log('working');
-console.log('another');
-
-// fetch('').then(i => {
-//     console.log(i);
-// })
 server.get('/:user/:repo', async (request) => {
     const { user, repo } = request.params as {user: string, repo: string};
     const numCommits = await axios.get(`https://api.github.com/repos/${user}/${repo}/commits?per_page=1`).then(i => i.headers.link?.replace('>; rel=\"last\"', '').match(/\d+$/)?.[0]);
